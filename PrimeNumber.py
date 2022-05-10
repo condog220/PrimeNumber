b8d6a1daff701995148e54297bee15fb934e69cb
condog220 <103192802+condog220@users.noreply.github.com>
2022-05-10 21:40:38 +0100

    Update PrimeNumber.py

--- PrimeNumber.py
+++ PrimeNumber.py
@@ -2,7 +2,7 @@
 for number in range(0, 99):
     # Prime number is always divisable by itself, therefore check it is greater than or equal to 2.
     if number >= 2:
-        # Our divisor; will go through the loop and mod number by i
+        # Our divisor; will go through the loop and MOD number by i
         for i in range(2, number):
             if number % 2 == 0:
                 # If any manage to be divisable with no remainder, number is not prime. Break
